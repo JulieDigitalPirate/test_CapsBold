b7d1d0eb474b1fe90865cfaf0fe16e9f4197c0d4
JulieV <Trenzelek2009@yandex.ru>
2023-05-31 15:44:39 +0300

    released confirmation window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Modal from './Modal';
+import useModal from './useModal';
 import './App.css';
 
-function App() {
+const App = () => {
+  const {toggle, visible} = useModal();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex', justifyContent: 'center'}}>
+      <button style={{marginLeft:"101px" , color:'#f9f8fa', background: 'linear-gradient(180deg, #bc9cff 0%, #8ba4f9 100%)', backgroundOrigin: 'border-box', border: '3px solid transparent', borderRadius: '10px', cursor: 'pointer',width: '300px',height: '80px', marginTop: '40px'}} type="button" onClick={toggle} onClick={toggle}>Show Confirmation</button>
+      <Modal visible={visible} toggle={toggle} />
     </div>
   );
 }
